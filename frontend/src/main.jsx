41470d5f5534d0b825e1045d5855ac8c54b38ca2
BigDataai-Dev <nizariounesdev@gmail.com>
2026-08-14 22:01:51 +0100

    style(supervision): load constrained desktop resolution layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import './styles/shell-v08.css';
 import './styles/operations-final-v08.css';
 import './styles/intervention-v08-layout.css';
 import './styles/stock-responsive-v08.css';
+import './styles/supervision-responsive-v08.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
