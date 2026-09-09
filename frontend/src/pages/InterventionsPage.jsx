80586ea168980ba83ad402afbfb36e2dc0f80835
ia-dev-talk <nizariounes@gmail.com>
2026-09-09 23:41:52 +0100

    feat(agent): route agent role to team review surface

--- frontend/src/pages/InterventionsPage.jsx
+++ frontend/src/pages/InterventionsPage.jsx
@@ -1,12 +1,18 @@
 import InterventionsWorkspace from './InterventionsWorkspace';
+import FieldAgentInterventionsPage from './FieldAgentInterventionsPage';
 import InterventionScopeBoundary from '../components/interventions/InterventionScopeBoundary';
 import { InterventionPermissionsProvider } from '../features/interventions/InterventionPermissionsContext.jsx';
+import { isFieldAgentRole } from '../features/field-agent/fieldAgentReview.js';
 
 export default function InterventionsPage(props) {
+  const content = isFieldAgentRole(props.userRole)
+    ? <FieldAgentInterventionsPage {...props} />
+    : <InterventionsWorkspace {...props} />;
+
   return (
     <InterventionPermissionsProvider userRole={props.userRole}>
       <InterventionScopeBoundary>
-        <InterventionsWorkspace {...props} />
+        {content}
       </InterventionScopeBoundary>
     </InterventionPermissionsProvider>
   );
